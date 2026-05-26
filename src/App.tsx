c029728e0cc8904ddb89855de5ad15ff00456827
agu.banchi <au.banchi@gmail.comgit config --global user.email agu.banchi@gmail.com>
2026-05-26 14:34:27 -0300

    creando los contenedor principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Form from "./components/Form"
 
 
 function App() {
@@ -5,7 +6,17 @@ function App() {
 
   return (
     <>
-     <h1 className="text-2xl">Contador de calorías y consumo</h1>
+    <header className="bg-teal-600 py-3">
+      <div className="max-w-4xl mx-auto flex justify-between">
+        <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorías</h1>
+      <button className="bg-teal-800 hover:bg-teal-900 hover:cursor-pointer rounded-lg text-white font-black p-3">Reiniciar App</button>
+      </div>
+    </header>
+    <section className="bg-teal-500 py-20 px-5">
+    <div className="max-w-4xl mx-auto">
+      <Form/>
+    </div>
+    </section>
     </>
   )
 }
