4d23965a248fdaaebefc3973f41605b6ba21ad55
agu.banchi <au.banchi@gmail.comgit config --global user.email agu.banchi@gmail.com>
2026-06-09 10:26:26 -0300

    Mostrando las calorías quemadas. Duplicamos la funcion pero cambiamos de categoria para que tome las de calorias quemadas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -9,6 +9,7 @@ export default function CalorieTracker({activities}: CalorieTrackersProps) {
   
   // contadores
 const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
+const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
     return (
     <>
     <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
@@ -17,6 +18,10 @@ const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => ac
     <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
         <span className="font-black text-6xl text-orange-400"> {caloriesConsumed}</span>
        Consumidas
+    </p>
+    <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+        <span className="font-black text-6xl text-green-400"> {caloriesBurned}</span>
+       Quemadas
     </p>
     </div>
    
