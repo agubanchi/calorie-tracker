5052e4763a53ce6d893b7fa093403d8714d46e0f
agu.banchi <au.banchi@gmail.comgit config --global user.email agu.banchi@gmail.com>
2026-06-09 10:17:00 -0300

    Mostrando calorias consumidas utilizando useMemo

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,11 +1,25 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
 
 type CalorieTrackersProps ={
     activities: Activity[]
 }
 
-export default function CalorieTracker({actvities}:CalorieTrackersProps) {
-  return (
-    <div>CalorieTracker</div>
+export default function CalorieTracker({activities}: CalorieTrackersProps) {
+  
+  // contadores
+const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
+    return (
+    <>
+    <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
+    
+    <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+    <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+        <span className="font-black text-6xl text-orange-400"> {caloriesConsumed}</span>
+       Consumidas
+    </p>
+    </div>
+   
+    </>
   )
 }
